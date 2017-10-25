ddd904341da541b2bdd254cd2fab6f07a88ee84a
Ken Hasenbank <Hasenbank.Ken@Gmail.com>
2017-10-25 10:44:57 +0200

    finally understood what i did -> add some comments

--- BugAlgorithm/BugAlgorithm/Bug0.cpp
+++ BugAlgorithm/BugAlgorithm/Bug0.cpp
@@ -34,18 +34,27 @@ bool Bug0::update(Box obstacle[], Box robot[], int nObst)
 		return true;
 	}
 
+	// test if we are near (dist <= MIN_DIST) an obstacle
+	// this function also sets a new heading along the obstacles wall if it returns true
 	int obs = obstacleInWay(obstacle, robot, nObst);
 
 	if (obs > -1) {
+		cout << "follow obstacle" << endl;
+		// activate emergency exit
 		getroffen = true;
 	}
 	else {
+		cout << "heading to goal" << endl;
+		// reset heading towards goal
 		headToGoal();
 	}
 
 	if (getroffen)
 	{
-		if (count > 1000){
+		if (count > 10000){
+			// we took a huge ammount of iterations
+			// assume the algorithm broken and exit
+			cout << "Fuck man, wo ist der Asgang..." << endl;
 			return true;
 		}
 		count++;
@@ -54,6 +63,7 @@ bool Bug0::update(Box obstacle[], Box robot[], int nObst)
 
 	
 	actPoint.Mac(heading, DIST_MIN); // move next step
+	// set the robots actual position
 	robot[0].Set(actPoint);
 	return false;
 }
@@ -61,15 +71,20 @@ bool Bug0::update(Box obstacle[], Box robot[], int nObst)
 int Bug0::obstacleInWay(Box obstacle[], Box robot[], int nObst)
 {
 	class ShadowMatrix3x3 { public: double m[3][3]; };   // To allow inlining
-	const Matrix3x3* Mat = (const Matrix3x3*)new ShadowMatrix3x3({0, -1, 0, 1, 0, 0, 0, 0, 1});
+	const Matrix3x3* Mat = (const Matrix3x3*)new ShadowMatrix3x3({0, -1, 0, 1, 0, 0, 0, 0, 1}); // simple 90° rotation Matrix
 
+	// this will point to the closest hitpoint of the obstacle
+	// should always be rectangular to the targets wall
 	Point out;
 	for (int i = 0; i <nObst; i++)
 	{
 		double dist = obstacle[i].distance(robot[0], &out);
+		// Run along the obstacles wall till the distance rises
 		if (dist <= DIST_MIN) {
+			// rotate hitpoint vector by 90 degree
 			Point out2 = out * *Mat;
 			out2.Normalize();
+			// should always be perpendicular to the obstavles wall
 			this->heading = out2;
 			return i;
 		}
