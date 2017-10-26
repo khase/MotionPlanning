c2bd1f2b775c184cfc59708969b80e3059652e4f
Ken Hasenbank <Hasenbank.Ken@Gmail.com>
2017-10-26 18:59:35 +0200

    implement update_cylinder

--- PotentialFieldMethod/Potential/Potential.cpp
+++ PotentialFieldMethod/Potential/Potential.cpp
@@ -7,7 +7,7 @@ using namespace std;
 static const double INKR = 0.01;        // step size for robot along gradient
 static const double DIST_MIN = 0.05;    // minimum distance between the robot and the goal
 static const double GOAL_ERROR = 0.01;  // distance between the robot and the goal
-static const double DIST_MIN_OBST = 0.1;    // distance when the obstacle interferes with the robot
+static const double DIST_MIN_OBST = 0.1;   // distance when the obstacle interferes with the robot
 static const double DIST_MIN_GOAL = 0.5;    // distance something
 static const double OBST_FORCE_SCALE = 0.00001;    // Magic do not touch
 static const double GOAL_FORCE_SCALE = 1;    // Magic do not touch
@@ -105,24 +105,44 @@ bool Potential::update_box(Box obstacle[], Box robot[], int nObst)
 /*************************************************************************************************************************/
 bool Potential::update_cylinder(Cylinder obstacle[], Cylinder robot[], int nObst)
 {
-    Point robotPos = actPoint;
-    static int cnt = 0;
 
-    cnt++;
+	Point robotPos = actPoint;
+	static int cnt = 0;
 
-    if (goalReached(robotPos, goalPosition, GOAL_ERROR))
-    {
-        actPoint = goalPosition;
-        cout << "at goal, smile :)\n";
-        
+	cnt++;
+
+	if (goalReached(robotPos, goalPosition, GOAL_ERROR))
+	{
+		actPoint = goalPosition;
+		cout << "at goal, smile :)\n";
+
 		return true;
-    }
+	}
 
+	Point globalForceVector = (goalPosition - robotPos);
+	double dGoal = robotPos.Distance(goalPosition);
 
+	if (dGoal > DIST_MIN_GOAL) {
+		globalForceVector = (DIST_MIN_GOAL * GOAL_FORCE_SCALE * globalForceVector) / dGoal;
+	}
 
-    actPoint.Mac((goalPosition - robotPos).Normalize(), INKR); // move next step
+	Point repulsive = Point(0, 0, 0);
+	for (int i = 0; i < nObst; i++) {
+		Cylinder obst = obstacle[i];
+		Point localForceVector;
+		double dist = robot[0].distance(obst, &localForceVector);
+		if (dist <= DIST_MIN_OBST) {
+			localForceVector = localForceVector.Normalize();
+			Point localRepulsive = OBST_FORCE_SCALE * ((1 / DIST_MIN_OBST) - (1 / dist)) * (1 / (dist * dist)) * localForceVector;
+			repulsive += localRepulsive;
+		}
+	}
 
-    return false;
+	globalForceVector += repulsive;
+
+	actPoint.Mac(globalForceVector.Normalize(), INKR); // move next step
+	robot[0].SetCenter(actPoint);
+	return false;
 }
 
 /*************************************************************************************************************************/
